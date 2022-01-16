0d606caed0c35fec6d4cef2a266b8f571f525b4f
Vedant Bassi <vedant.bassi@signzy.com>
2022-01-16 06:40:48 +0530

    added better error handling for functional test

--- functionaltest_driver.py
+++ functionaltest_driver.py
@@ -34,8 +34,31 @@ if __name__=="__main__":
 	failed_cases_count = 0
 
 	for testcase in testcases_list:
-	
-		api_hit_result = api_controller.functional_api_runner(testcase,request_list)
+		
+		try:
+			api_hit_result = api_controller.functional_api_runner(testcase,request_list)
+
+		except Exception as e:
+			logger.error("Error in processing request.")
+
+			api_hit_result = {}
+
+			api_hit_result["testcaseName"] = testcase["testcaseName"]
+			api_hit_result["testcaseID"] = str(testcase["_id"])
+			api_hit_result["totalResponseTime"] = 0
+			api_hit_result["individualResponseTimes"] = []
+			api_hit_result["remarks"] = "Error while executing request, fix testcase."
+			api_hit_result["result"] = False
+
+
+			for i in range(1,len(request_list)+1):
+				api_hit_result["requestBody"+str(i)] = ""
+				api_hit_result["expectedResponseCode"+str(i)] = ""
+				api_hit_result["receivedResponseCode"+str(i)] = ""
+				api_hit_result["expectedResponseTime"+str(i)] = ""
+				api_hit_result["receivedResponseTime"+str(i)] = ""
+				api_hit_result["expectedResponseBody"+str(i)] = ""
+				api_hit_result["receivedResponseBody"+str(i)] = ""
 
 		api_hit_result["testID"] = testID
 
@@ -50,13 +73,15 @@ if __name__=="__main__":
 		logger.info("api_hit_result")
 		logger.info(api_hit_result)
 
-
 		logger.info(f"Passed test cases: {passed_cases_count}")
 		logger.info(f"Failed test cases: {failed_cases_count}")
 
 		dbops.update_test(testID,"passedCasesCount",passed_cases_count)
 		dbops.update_test(testID,"failedCasesCount",failed_cases_count)
-	
+
+		
+
+
 	end_time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 
 	dbops.update_test(testID,"endTime",end_time)
